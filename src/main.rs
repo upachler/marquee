67bb47a68d688c7bd0c188881f97520bffc3dc25
uwe_pachler <uwe.pachler@gmail.com>
2019-10-03 11:41:25 +0200

    apply rustfmt to code

--- src/main.rs
+++ src/main.rs
@@ -5,57 +5,55 @@ extern crate rocket;
 
 use rocket::State;
 use rocket_contrib::json::*;
-use serde::{Serialize,Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct UserInfo {
-    first_name : String,
-    last_name : String,
-    roles : Vec<String>,
+    first_name: String,
+    last_name: String,
+    roles: Vec<String>,
 }
 
 #[derive(Serialize, Clone)]
 struct SiteReference {
-    display_name : String,
-    url : String,
-    required_roles : Vec<String>,
+    display_name: String,
+    url: String,
+    required_roles: Vec<String>,
 }
 
 #[derive(Serialize)]
 struct Overview {
-    user_info : UserInfo,
-    site_refs : Vec<SiteReference>,
+    user_info: UserInfo,
+    site_refs: Vec<SiteReference>,
 }
 
-
 struct Context {
-        all_sites : Vec<SiteReference>
+    all_sites: Vec<SiteReference>,
 }
 
 #[get("/")]
-fn index (ctx : State<Context>) -> Json<Overview> {
+fn index(ctx: State<Context>) -> Json<Overview> {
     Json(Overview {
-        user_info : UserInfo {
-            first_name : String::from("Mickey"),
-            last_name : String::from("Mouse"),
-            roles : vec![String::from("disney")],
+        user_info: UserInfo {
+            first_name: String::from("Mickey"),
+            last_name: String::from("Mouse"),
+            roles: vec![String::from("disney")],
         },
-        site_refs : vec![ctx.all_sites[0].clone()],
+        site_refs: vec![ctx.all_sites[0].clone()],
     })
 }
 
-
 fn main() {
-    let context : Context = Context{
-        all_sites : vec![SiteReference {
+    let context: Context = Context {
+        all_sites: vec![SiteReference {
             url: String::from("http://disney.com"),
-            display_name : String::from("Disney"),
-            required_roles : vec![String::from("disney")]
-        }]
+            display_name: String::from("Disney"),
+            required_roles: vec![String::from("disney")],
+        }],
     };
 
     rocket::ignite()
-    .manage(context)
-    .mount("/", routes![index])    
-    .launch();
+        .manage(context)
+        .mount("/", routes![index])
+        .launch();
 }
