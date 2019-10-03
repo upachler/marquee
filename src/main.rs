c96a04eecb62efd6bbbeae246dbaab1aaeca15e9
uwe_pachler <uwe.pachler@gmail.com>
2019-10-03 18:21:27 +0200

    add rocket_oauth2 and some preliminary logic

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,16 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::State;
+use rocket::http::*;
+use rocket::request::*;
+use rocket::response::*;
+use rocket::*;
 use rocket_contrib::json::*;
+use rocket_oauth2::hyper_sync_rustls_adapter::HyperSyncRustlsAdapter;
+use rocket_oauth2::{Callback, OAuth2, OAuthConfig, Provider, TokenResponse};
 use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
+use std::path::PathBuf;
 
 #[derive(Serialize, Deserialize)]
 struct UserInfo {
@@ -43,8 +50,42 @@ fn index(ctx: State<Context>) -> Json<Overview> {
     })
 }
 
+#[get("/<path..>")]
+fn auth_fallback(path: PathBuf) -> Redirect {
+    Redirect::to("do-login")
+}
+
+fn token_callback(
+    request: &Request,
+    token: TokenResponse,
+) -> std::result::Result<Redirect, Box<::std::error::Error>> {
+    let mut cookies = request.guard::<Cookies>().expect("request cookies");
+
+    // Set a private cookie with the access token
+    cookies.add_private(
+        Cookie::build("token", token.access_token)
+            .same_site(SameSite::Lax)
+            .finish(),
+    );
+    Ok(Redirect::to("/"))
+}
+
 fn main() {
-    let context: Context = Context {
+    let oauth_provider = Provider {
+        auth_uri: Cow::from(
+            "http://localhost:8080/auth/realms/marquee/protocol/openid-connect/auth",
+        ),
+        token_uri: Cow::from(
+            "http://localhost:8080/auth/realms/marquee/protocol/openid-connect/token",
+        ),
+    };
+    let oauth_config = OAuthConfig::new(
+        oauth_provider,
+        String::from("marquee"),
+        String::from(""),
+        String::from("/exchange-token"),
+    );
+    let context = Context {
         all_sites: vec![SiteReference {
             url: String::from("http://disney.com"),
             display_name: String::from("Disney"),
@@ -54,6 +95,13 @@ fn main() {
 
     rocket::ignite()
         .manage(context)
-        .mount("/", routes![index])
+        .mount("/", routes![index, auth_fallback])
+        .attach(OAuth2::custom(
+            HyperSyncRustlsAdapter,
+            token_callback,
+            oauth_config,
+            "/exchange-token",
+            Some(("/do-login", vec!["roles".to_string()])),
+        ))
         .launch();
 }
